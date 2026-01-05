09f498fd0afd4ae289d103052d33a07653ed1747
Fatty <aaron.kondal1@gmail.com>
2026-01-05 22:04:08 +1100

    Create questionDetails object and generate it with styling

---
+++ scripts/questions.js
@@ -0,0 +1,33 @@
+import { questions, answers } from "./flashcard.js";
+
+
+const questionDetails = [{
+  title: 'Math Questions',
+  question: 'What is 1 + 1?'
+},
+
+{
+  title: 'Math Questions',
+  question: 'What is 2 + 1?'
+},
+
+{
+  title: 'Math Questions',
+  question: 'What is 3 + 1?'
+}];
+
+questionDetails.forEach((question) => {
+  const div = document.createElement('div');
+
+  div.textContent = question.title
+
+  questions.push(div);
+});
+
+let html = '';
+
+questionDetails.forEach(question => {
+  html += `<div class="flashcard">${question.title}, ${question.question}</div>`;
+});
+
+document.getElementById('main').innerHTML = html;
