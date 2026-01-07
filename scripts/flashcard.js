5871c22b80d7dd9685ba35bb5caabb1a73e661ce
Fatty <aaron.kondal1@gmail.com>
2026-01-08 10:44:27 +1100

    Fixed a memory leak with the html variable. Also chnaged style of how card will be displayed and added a scroll left and right button.

--- scripts/flashcard.js
+++ scripts/flashcard.js
@@ -1,6 +1,5 @@
 import { questions, answers } from "./mainArrayHolder.js";
 
-
 const questionDetails = [{
   title: 'Math Questions',
   question: 'What is 1 + 1?'
@@ -16,14 +15,41 @@ const questionDetails = [{
   question: 'What is 3 + 1?'
 }];
 
-let html = '';
+let currentCardIndex = 0;
 
-questionDetails.forEach((question, index) => {
-  html += `
-    <div class="card card-${index}">
-      ${question.title}, ${question.question}
+renderCard();
+
+function renderCard() {
+  const questionIndexArray = questionDetails[currentCardIndex];
+
+  document.getElementById('main').innerHTML = `
+    <div class="card card-${currentCardIndex}">
+      ${questionIndexArray.title}, ${questionIndexArray.question}
     </div>
   `;
+}
+
+
+const leftButton = document.querySelector('.js-scroll-left');
+const rightButton = document.querySelector('.js-scroll-right');
+
+
+leftButton.addEventListener('click', () => {
+  currentCardIndex--;
+
+  if (currentCardIndex < 0) {
+    currentCardIndex = questionDetails.length - 1;
+  }
+
+  renderCard();
 });
 
-document.getElementById('main').innerHTML = html;
+rightButton.addEventListener('click', () => {
+  currentCardIndex++;
+
+  if (currentCardIndex >= questionDetails.length) {
+    currentCardIndex = 0;
+  }
+
+  renderCard();
+});
